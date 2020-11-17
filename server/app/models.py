a4442eb16fe452c39c351edc09751df98a91e85f
Kyle <kylehorton33@gmail.com>
2020-11-17 13:47:42 -0500

    add new model for category

--- server/app/models.py
+++ server/app/models.py
@@ -1,10 +1,15 @@
 from app import db
 from datetime import datetime
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    items = db.relationship('Item', backref='category', lazy=True)
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    category = db.Column(db.String(30), default='UNCATEGORIZED')
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False, default=1)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
