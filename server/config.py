64beef878d5727c228b0bc8e76552e6878991046
Kyle <kylehorton33@gmail.com>
2020-11-17 11:44:27 -0500

    update db name

--- server/config.py
+++ server/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'grocery_list.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
